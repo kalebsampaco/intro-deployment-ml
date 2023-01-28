5e0bf2ffd88619d5448863e5170e0e5ceea3839b
Caleb <kalebsam@hotmail.com>
2023-01-28 16:09:28 -0500

    fixes

---
+++ pruebas_endpoint.py
@@ -0,0 +1,18 @@
+import requests
+
+
+url = 'https://deployment-intro-ml-service-mujcwzvn5a-uc.a.run.app/vi/prediction'
+
+json = {
+        "opening_gross": 8330681,
+        "screens": 2271,
+        "production_budget": 13000000,
+        "title_year": 1999,
+        "aspect_ratio": 1.85,
+        "duration": 97,
+        "cast_total_facebook_likes": 37907,
+        "budget": 16000000,
+        "imdb_score": 7.2
+        }
+
+breakpoint()
